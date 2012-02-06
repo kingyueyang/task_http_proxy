483652caad91e95df868199a07134ebfdef6f09a
yueyang <bj.yueyang@gmail.com>
2012-02-06 17:48:42 +0800

    add project file add event queue

---
+++ src/ev_queue.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ev_queue.c
+ *
+ *    Description:  post event queue
+ *
+ *        Version:  1.0
+ *        Created:  02/06/2012 05:35:27 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YueYang (feinno), yueyang@feinno.com
+ *        Company:  feinno
+ *
+ * =====================================================================================
+ */
+
+#include "ev_queue.h"
+
---
+++ src/ev_queue.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ev_queue.h
+ *
+ *    Description:  post event queue
+ *
+ *        Version:  1.0
+ *        Created:  02/06/2012 05:36:34 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YueYang (feinno), yueyang@feinno.com
+ *        Company:  feinno
+ *
+ * =====================================================================================
+ */
+
+#ifndef _EV_QUEUE_H_
+#define _EV_QUEUE_H_
+
+#include <stdlib.h>
+#include <string.h>
+
+#endif
+
